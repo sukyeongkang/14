112346ccc8779960c4d8ac053de0dfa7aba65a7f
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-12-06 16:35:16 +0900

    실습 2

---
+++ main2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Book{
+	int number;
+	char title[10];
+	
+};
+
+void main(void){
+	struct Book *p;
+	
+	p = (struct Book*)malloc(2*sizeof(struct Book));
+	
+	
+	if ( p==NULL){
+		printf("메모리 할당 오류\n");
+		return -1;
+	}
+	
+	p->number =1;
+	strcpy(p->title, "C Programming");
+	
+	(p+1)-> number = 2;
+	strcpy((p+1)->title, "Electronics");
+	
+	free(p);
+	return;
+
+}
